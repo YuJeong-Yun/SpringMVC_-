0988b4653e1e94a02869884ecf0ab1745a04b2c1
yyj <yunyj99@naver.com>
2023-01-26 04:02:55 +0900

    ch2-22 redirect와 forward

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,37 +1,39 @@
 package com.fastcampus.ch2;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller 
+@Controller
 public class RegisterController {
 //	@RequestMapping("/register/add")
 //	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm"; // WEB-INF/vies/registerForm.jsp
-//	}
-	
+	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
+	public String register() {
+		return "registerForm"; // WEB-INF/vies/registerForm.jsp
+	}
+
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
-	@PostMapping("/register/save")  //4.3
+	@PostMapping("/register/save") // 4.3
 	public String save(User user, Model m) throws Exception {
 		// 1.유효성 검사
-		if(!isValid(user)) {
-			String msg=URLEncoder.encode("id를 잘못 입력하셨습니다.","utf-8");
-			
-			m.addAttribute("msg",msg);
-			return "redirect:/register/add";
+		if (!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+
+			m.addAttribute("msg", msg);
+			return "forward:/register/add";
 //			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
 		}
-		
-		// 2.DB에 신규회원 정보를 저장 
+
+		// 2.DB에 신규회원 정보를 저장
 		return "registerInfo";
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
