9a5c1d96364ac088a1ce68e2709e25614bfcddac
yyj <yunyj99@naver.com>
2023-01-30 02:49:19 +0900

    ch2-28 예외처리(2)-이론

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,14 +2,16 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
-
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 500
 	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex, Model m) {
 		m.addAttribute("ex", ex);
--- ch2/src/main/java/com/fastcampus/ch2/GlobalCatcher.java
+++ ch2/src/main/java/com/fastcampus/ch2/GlobalCatcher.java
@@ -6,14 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice("com.fastcampus.ch2") // 지정된 패키지에서 발생한 예외만 처 
+@ControllerAdvice("com.fastcampus.ch2") // 지정된 패키지에서 발생한 예외만 처리 
 //@ControllerAdvice // 모든 패키지에 처리 
 public class GlobalCatcher {
-	@ExceptionHandler(Exception.class)
-	public String catcher(Exception ex, Model m) {
-		m.addAttribute("ex", ex);
-		return "error";
-	}
+//	@ExceptionHandler(Exception.class)
+//	public String catcher(Exception ex, Model m) {
+//		m.addAttribute("ex", ex);
+//		return "error";
+//	}
 
 	@ExceptionHandler({ NullPointerException.class, FileNotFoundException.class })
 	public String catcher2(Exception ex, Model m) {
