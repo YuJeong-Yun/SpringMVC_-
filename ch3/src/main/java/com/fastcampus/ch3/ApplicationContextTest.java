9863b23420032ff547e792f74e1afd36951e7917
yyj <yunyj99@naver.com>
2023-03-27 01:59:51 +0900

    ch3-8 Spring DI 활용하기 - 이론(4)

--- ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
+++ ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -38,7 +38,7 @@ class Car {
     //    @Autowired
     Door[] doors;
 
-//    @Autowired
+    //    @Autowired
     public Car(@Value("red") String color, @Value("100") int oil, Engine engine, Door[] doors) {
         this.color = color;
         this.oil = oil;
@@ -57,6 +57,32 @@ class Car {
     }
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo {
+    @Value("#{systemProperties['user.timezone']}")
+    String timeZone;
+    @Value("#{systemEnvironment['PWD']}")
+    String currDir;
+    @Value("${autosaveDir}")
+    String autosaveDir;
+    @Value("${autosaveInterval}")
+    int autosaveInterval;
+    @Value("${autosave}")
+    boolean autosave;
+
+    @Override
+    public String toString() {
+        return "SysInfo{" +
+                "timeZone='" + timeZone + '\'' +
+                ", currDir='" + currDir + '\'' +
+                ", autosaveDir='" + autosaveDir + '\'' +
+                ", autosaveInterval=" + autosaveInterval +
+                ", autosave=" + autosave +
+                '}';
+    }
+}
+
 public class ApplicationContextTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
@@ -64,5 +90,9 @@ public class ApplicationContextTest {
         Car car = (Car) ac.getBean("car"); // 이름으로 빈 검색
         Car car2 = (Car) ac.getBean(Car.class);   // 타입으로 빈 검색
         System.out.println("car = " + car);
+
+        System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+        Map<String, String> map = System.getenv();
+        System.out.println("map = " + map);
     }
 }
